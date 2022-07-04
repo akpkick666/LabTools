d0877e03bd9bd62a0a8a3fe369396b0e24def101
akpkick666 <akpkick666@gmail.com>
2022-07-04 12:37:49 +0900

    Pythonディレクトリ追加

--- app/Http/Controllers/ExecController.php
+++ app/Http/Controllers/ExecController.php
@@ -15,7 +15,7 @@ class ExecController extends Controller
     }
 
     public function cdpython(Request $request) {
-        $command = "cd /Users/akp_kick6/development/research/test && python cd_1.py";
+        $command = "cd /Users/akp_kick6/development/LabTools/app/Http/Python && python cd_1.py";
         exec($command, $output);
     }
 
