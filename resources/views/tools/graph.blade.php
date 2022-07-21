27fc19286c6eb15f71812225c10451528ad45d90
akpkick666 <akpkick666@gmail.com>
2022-07-21 16:25:49 +0900

    graph.blade作成,グラフ画像表示

--- resources/views/tools/graph.blade.php
+++ resources/views/tools/graph.blade.php
@@ -2,14 +2,15 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>Blog</title>
+        <title>KurodaLab</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>CD</h1>
+        <h1>RESULT</h1>
         <div>
             <h2 class='title'>{{ $graph_name }}</h2>
+            <img src="{{ asset('img/cd_graph/' . $graph_name . '.png')}}" alt="">
         </div>
     </body>
 </html>
