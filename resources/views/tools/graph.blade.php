c27fedadb57dd2eb1450a5e0946f9abea28426d7
akpkick666 <akpkick666@gmail.com>
2022-07-29 15:51:14 +0900

    グラフ画面メタデータ追加

--- resources/views/tools/graph.blade.php
+++ resources/views/tools/graph.blade.php
@@ -5,12 +5,18 @@
         <title>KurodaLab</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- Bootstrap -->
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </head>
     <body>
-        <h1>RESULT</h1>
-        <div>
-            <h2 class='title'>フォルダ名:  . {{ $data_dir }}</h2>
-            <img src="{{ asset('img/cd/' . $data_dir . '/graph_data.png')}}" alt="">
+        <div class="container">
+            <h1>RESULT</h1>
+            <div>
+                <h2 class='title'>フォルダ名:  . {{ $data_dir }}</h2>
+                <img src="{{ asset('img/cd/' . $data_dir . '/graph_data.png')}}" alt="">
+                <img src="{{ asset('img/cd/' . $data_dir . '/sample_metadata.png')}}" alt="" class="w-25">
+            </div>
         </div>
     </body>
 </html>
